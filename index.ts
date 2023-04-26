097b249738ea1805fcadb3d76fa14d56160c7175
chrisfs313 <chris_fs313@hotmail.com>
2023-04-26 18:37:50 -0500

    tweaks

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from './src/shute-technologies/helpers/stHelpers';
 export * from './src/shute-technologies/interfaces/stInterfaces';
+export * from './src/shute-technologies/interfaces/stDebugConsole';
 export * from './src/shute-technologies/utils/stUtils';
 export * from './src/external-libs/base64-binary';
