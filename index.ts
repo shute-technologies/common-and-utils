788a40101feac095b204b41706083def44a26303
chrisfs313 <chris_fs313@hotmail.com>
2023-04-26 21:01:05 -0500

    missing export

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+export * from './src/shute-technologies/enums/stEnumHttpErrorCode';
 export * from './src/shute-technologies/helpers/stHelpers';
 export * from './src/shute-technologies/interfaces/stInterfaces';
 export * from './src/shute-technologies/interfaces/stDebugConsole';
