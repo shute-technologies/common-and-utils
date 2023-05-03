80d2d75c9f828e1bc42490174042ccee8fe7c3eb
chrisfs313 <chris_fs313@hotmail.com>
2023-05-03 12:14:16 -0500

    updating interfaces

--- src/shute-technologies/interfaces/stInterfaces.ts
+++ src/shute-technologies/interfaces/stInterfaces.ts
@@ -1,10 +1,14 @@
 export type IFunction0<TResult> = () => TResult;
 export type IFunction1<T, TResult> = (args?: T) => TResult;
-export type IRqFunction1<T, TResult> = (args: T) => TResult;
 export type IFunction2<T0, T1, TResult> = (a0?: T0, a1?: T1) => TResult;
 export type IFunction3<T0, T1, T2, TResult> = (a0?: T0, a1?: T1, a2?: T2) => TResult;
 export type IFunction4<T0, T1, T2, T3, TResult> = (a0?: T0, a1?: T1, a2?: T2, a3?: T3) => TResult;
 export type IFunction5<T0, T1, T2, T3, T4, TResult> = (a0?: T0, a1?: T1, a2?: T2, a3?: T3, a4?: T4) => TResult;
+export type IRFunction1<T, TResult> = (args: T) => TResult;
+export type IRFunction2<T0, T1, TResult> = (a0: T0, a1: T1) => TResult;
+export type IRFunction3<T0, T1, T2, TResult> = (a0: T0, a1: T1, a2: T2) => TResult;
+export type IRFunction4<T0, T1, T2, T3, TResult> = (a0: T0, a1: T1, a2: T2, a3: T3) => TResult;
+export type IRFunction5<T0, T1, T2, T3, T4, TResult> = (a0: T0, a1: T1, a2: T2, a3: T3, a4: T4) => TResult;
 
 export type ICallback0 = () => void;
 export type ICallback1<T> = (args?: T) => void;
@@ -12,6 +16,11 @@ export type ICallback2<T0, T1> = (a0?: T0, a1?: T1) => void;
 export type ICallback3<T0, T1, T2> = (a0?: T0, a1?: T1, a2?: T2) => void;
 export type ICallback4<T0, T1, T2, T3> = (a0?: T0, a1?: T1, a2?: T2, a3?: T3) => void;
 export type ICallback5<T0, T1, T2, T3, T4> = (a0?: T0, a1?: T1, a2?: T2, a3?: T3, a4?: T4) => void;
+export type IRCallback1<T> = (args: T) => void;
+export type IRCallback2<T0, T1> = (a0: T0, a1: T1) => void;
+export type IRCallback3<T0, T1, T2> = (a0: T0, a1: T1, a2: T2) => void;
+export type IRCallback4<T0, T1, T2, T3> = (a0: T0, a1: T1, a2: T2, a3: T3) => void;
+export type IRCallback5<T0, T1, T2, T3, T4> = (a0: T0, a1: T1, a2: T2, a3: T3, a4: T4) => void;
 
 export interface IDictionary<T> { [Key: string]: T; }
 export type IConstructor<TClassDefinition> = new (...args: unknown[]) => TClassDefinition;
